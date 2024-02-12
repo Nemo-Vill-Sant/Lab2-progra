6fd564a0468b66469761eee103b1d91330640b1f
Nohemy-0503 <110186577+Nohemy-0503@users.noreply.github.com>
2024-02-11 22:51:39 -0600

    Update P.lab2.js

--- P.lab2.js
+++ P.lab2.js
@@ -36,3 +36,29 @@ document.getElementById('click-ejemplo1').addEventListener('click', clickTocar1)
 document.getElementById('click-ejemplo2').addEventListener('click', clickTocar2);
 document.getElementById('click-ejemplo3').addEventListener('click', clickTocar3);
 
+// Focus
+function handleFocus1() {
+    const focusInput1 = document.getElementById('focus-1').querySelector('.focus-input');
+    focusInput1.style.backgroundColor = 'red';
+}
+
+function handleFocus2() {
+    const hiddenText = document.getElementById('hidden-text');
+    hiddenText.style.display = 'block';
+}
+
+function handleFocus3() {
+    const focusBox3 = document.getElementById('focus-3');
+    focusBox3.style.backgroundColor = 'lightgreen';
+    const focusInput3 = focusBox3.querySelector('.focus-input');
+    focusInput3.style.backgroundColor = '#fff'; 
+}
+
+document.getElementById('focus-1').querySelector('.focus-input').addEventListener('focus', handleFocus1);
+document.getElementById('focus-2').querySelector('input').addEventListener('focus', handleFocus2);
+document.getElementById('focus-3').querySelector('.focus-input').addEventListener('focus', handleFocus3);
+
+// Nombres y códigos de los integrantes
+const footer = document.createElement('footer');
+footer.innerHTML = 'Integrantes: Arnold David Villatoro Santos - SMSS032621, Marcela Nohemy Hernández Villatoro - SMSS072922';
+document.body.appendChild(footer);
